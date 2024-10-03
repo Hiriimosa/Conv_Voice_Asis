363e2464f9554338882af72522c6f9dc7bfd82c8
Maxim Shangareev <tegiperxxl@gmail.com>
2024-10-04 00:13:07 +0500

    Удалены файлы с ~ и добавлено правило в .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Conv_Voice_Asis',
+    version='03.10.2024',
+    packages=[''],
+    url='',
+    license='',
+    author='Shangareev Maxim (Hiriimosa)',
+    author_email='tegiperxxl@gmail.com',
+    description=''
+)
